56386889690aa652c378bc97021e80f466631f8d
unknown <you@example.com>
2026-09-03 14:56:58 +0530

    fix: actually inject password in backend cache.mjs and bump to v39

--- src/AuctionMQTT.cpp
+++ src/AuctionMQTT.cpp
@@ -112,6 +112,10 @@ void AuctionMQTT::handleMessage(char* topic, byte* payload, unsigned int length)
                     a.Items_Count      = auc["Items_Count"].as<int>();
                     a.Registered_Count = auc["Registered_Count"].as<int>();
                     a.Password         = auc["Password"] | "";
+                    Serial.print("Parsed Password from MQTT for ");
+                    Serial.print(a.Name);
+                    Serial.print(": ");
+                    Serial.println(a.Password);
                     a.Password         = auc["Password"] | "";
                     lastResponse.Auctions.push_back(a);
                 }
